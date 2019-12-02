af845b0d3cfe8e7daa16c8fc0cc1835842dd6c42
Matej <mb7628@student.uni-lj.si>
2019-12-02 20:39:41 +0100

    bugfix

--- api/v1/src/main/java/si/rso/cart/api/endpoints/ShoppingCartEndpoint.java
+++ api/v1/src/main/java/si/rso/cart/api/endpoints/ShoppingCartEndpoint.java
@@ -13,7 +13,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/shopping-carts")
+@Path("/shopping-cart")
 @RequestScoped
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
