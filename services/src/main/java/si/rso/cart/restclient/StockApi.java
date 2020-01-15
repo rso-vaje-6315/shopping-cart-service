1c6a4c903a49888a0ebf9557270f6e710fd5dd7c
Matej <mb7628@student.uni-lj.si>
2020-01-15 21:49:47 +0100

    popravek rest clienta

--- services/src/main/java/si/rso/cart/restclient/StockApi.java
+++ services/src/main/java/si/rso/cart/restclient/StockApi.java
@@ -1,10 +1,13 @@
 package si.rso.cart.restclient;
 
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@RegisterRestClient
 public interface StockApi {
 
     @GET
